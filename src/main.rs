961bf26680007af7984f21e13d221733532fab75
triistam <isakulovdev@gmail.com>
2023-07-26 12:19:48 +0500

    Added AsciiResponse and QueryParams to make it easier to work with

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,51 @@
-use tapciify::{image_to_ascii, DEFAULT_ASCII_STRING, DEFAULT_FONT_RATIO};
-use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+use std::{fs::File, io::{self, Write}};
+use std::fs::OpenOptions;
 
-#[get("/")]
-async fn hello() -> impl Responder {
-    let image = image::open("./assets/original.png").unwrap();
-    let ascii_image = image_to_ascii(image, 64, DEFAULT_ASCII_STRING, false, DEFAULT_FONT_RATIO);
-    
-    let result = ascii_image.result;
-    
-    HttpResponse::Ok().body(result)
+
+use axum::{routing::get, Router, Json, extract::Query};
+use serde_json::{Value,json};
+
+use tapciify::{image_to_ascii, DEFAULT_ASCII_STRING, DEFAULT_FONT_RATIO};
+
+mod structs;
+
+use structs::{AsciiResponse, QueryParams};
+
+#[tokio::main]
+async fn main(){
+    let app = Router::new()
+        .route("/", get(hello_world));
+    let host = "http://localhost:3000";
+
+    // run it with hyper on localhost:3000
+    axum::Server::bind(&"127.0.0.1:3000".parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }
 
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .service(hello)
-    })
-    .bind(("127.0.0.1", 8080))?
-    .run()
-    .await
+async fn download_image(image_link: String) -> String {
+    let image_path = "./assets/image.png";
+    let mut file = OpenOptions::new().read(true).write(true).open(image_path).expect("Could not load");
+    let img_bytes = reqwest::get(image_link).await.expect("Could not load")
+    .bytes().await.expect("Could not");
+
+    file.write_all(&img_bytes).expect("COuld nit");
+
+    image_path.to_string()
+}
+
+async fn hello_world(query: Query<QueryParams>) -> Json<AsciiResponse> {
+    let image = image::open(download_image(query.image.clone()).await);
+
+    let ascii_img = image_to_ascii(
+        image.unwrap(),
+        query.width,
+        DEFAULT_ASCII_STRING, 
+        false, 
+        DEFAULT_FONT_RATIO);
+    println!("{}",&query.image);
+    
+
+    Json(AsciiResponse { message: (ascii_img.result) })
 }
