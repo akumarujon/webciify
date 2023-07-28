d5289584c55a498b5103e06536ce007b01ccafa5
triistam <isakulovdev@gmail.com>
2023-07-28 11:12:35 +0500

    Removed unused imports, formatted code and added the feature 'use memory instead of storage' by Russian boy @tapnisu

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,32 @@
-use std::io::Write;
-use std::fs::OpenOptions;
-
-
+use std::path::PathBuf;
 use axum::{routing::get, Router, Json, extract::Query};
-
 use tapciify::{image_to_ascii, DEFAULT_ASCII_STRING, DEFAULT_FONT_RATIO};
-
 mod structs;
-
 use structs::{AsciiResponse, QueryParams};
 
-#[tokio::main]
-async fn main(){
-    let app = Router::new()
+
+#[shuttle_runtime::main]
+async fn axum(#[shuttle_static_folder::StaticFolder(folder = "assets")] public_folder: PathBuf) -> shuttle_axum::ShuttleAxum {
+    println!("{:?}", &public_folder);
+
+    let router = Router::new()
         .route("/", get(send_ascii));
-    // run it with hyper on localhost:3000
-    axum::Server::bind(&"127.0.0.1:3000".parse().unwrap())
-        .serve(app.into_make_service())
-        .await
-        .unwrap();
-}
 
-async fn download_image(image_link: String) -> String {
-    let image_path = "./assets/image.png";
-    let mut file = OpenOptions::new().read(true).write(true).open(image_path).expect("Could not load");
-    let img_bytes = reqwest::get(image_link).await.expect("Could not load")
-    .bytes().await.expect("Could not");
-
-    file.write_all(&img_bytes).expect("COuld nit");
-
-    image_path.to_string()
+    Ok(router.into())
 }
 
 async fn send_ascii(query: Query<QueryParams>) -> Json<AsciiResponse> {
-    let image = image::open(download_image(query.image.clone()).await);
+    let img_bytes = reqwest::get(&query.image).await.unwrap().bytes().await.unwrap();
 
+    let image = image::load_from_memory(&img_bytes).unwrap();
+    
     let ascii_img = image_to_ascii(
-        image.unwrap(),
+        image,
         query.width,
         DEFAULT_ASCII_STRING, 
         false, 
         DEFAULT_FONT_RATIO);
-    println!("{}",&query.image);
+    println!("{}", &query.image);
     
 
     Json(AsciiResponse { message: (ascii_img.result) })
